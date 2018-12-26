ce18b8fcf4f4166682375fe12381a136b8263a77
ibrahimsebbane <ibrahim.sebbane@gmail.com>
2018-12-26 23:31:18 +0100

    nothing special

--- resources/views/front/projets/projetsDetails.blade.php
+++ resources/views/front/projets/projetsDetails.blade.php
@@ -17,18 +17,114 @@
 @section('content')
 
 	<br/> <br/>
-	
-	<div class="container">
+  <div class="container shadow p-3 mb-5 bg-white rounded">
+	<div class="row">
+      <div class="col-md-12">
+           <div class="box box-primary">
 
-	<div class="card border-dark mb-3" style="max-width: 18rem;">
- 		<div class="card-header">Header</div>
- 		<div class="card-body text-dark">
-    	<h5 class="card-title">Dark card title</h5>
-    	<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  		</div>
-	</div>
+            <div class="box-header with-border">
+              <h3 class="box-title">Informations</h3>
+            </div>
+            <!-- /.box-header -->
+              <div class="box-body">
+                <div class="row container ">
+                  <div class="col-md-3">
+                    <strong>Titre</strong>
+                  </div>
+                  <div class="col-md-9">
+                    <p class="text-muted">
+                      {{$projet->intitule}}
 
+                    </p>
+                  </div>
+                </div>
+                <div class="row container">
+                  <div class="col-md-3">
+                    <strong>Résumé</strong>
+                  </div>
+                  <div class="col-md-9">
+                    <p class="text-muted">
+                     {{$projet->resume}}
+                    </p>
+                  </div>
+                </div>
+                <div class="row container">
+                  <div class="col-md-3">
+                    <strong>Type</strong>
+                  </div>
+                  <div class="col-md-9">
+                    <p class="text-muted">
+                      {{$projet->type}}
+                    </p>
+                  </div>
+                </div>
 
-	</div>
+                  <strong><i class="margin-r-5"></i></strong>
+                  <hr>
+
+                  <div class="col-md-3">
+                    <strong><i class="fa fa-group  margin-r-5"></i>Partenaires</strong>
+                  </div>
+                  <div class="col-md-9">
+                    <p class="text-muted">
+                      {{$projet->partenaires}}
+                    </p>
+                  </div>
+
+                  <strong><i class="margin-r-5"></i></strong>
+                <hr>
+                <div class="col-md-3">
+                  <strong><i class="fa fa-user  margin-r-5"></i>Chef du projet</strong>                
+                 </div>
+                  <div class="col-md-9">
+                    <a href="{{url('membres/'.$projet->chef_id.'/details')}}">{{$projet->chef->name}} {{$projet->chef->prenom}}</a>
+                  </div>
+
+                <strong><i class="margin-r-5"></i></strong>
+                  <hr>
+
+                <div class="col-md-3">
+                  <strong><i class="fa fa-group  margin-r-5"></i>Membres du projet</strong>                
+                 </div>
+                  <div class="col-md-9">
+                    @foreach($membres as $membre)
+                    <ul>
+                        <li><a href="{{url('membres/'.$membre->id.'/details')}}">{{ $membre->name }} {{ $membre->prenom }}</a></li>
+                    </ul>
+                    @endforeach
+
+                  </div>  
+
+                  <strong><i class="margin-r-5"></i></strong>
+                <hr>
+                @if($projet->lien)
+                <div class="row">
+                <div class="col-md-3">
+                  <strong><i class="fa fa-link  margin-r-5"></i>Lien</strong>                
+                 </div>
+                  <div class="col-md-9">
+                    <a href="#">{{$projet->lien}}</a>
+                  </div> 
+                </div> 
+                @endif
+                @if($projet->detail)
+                <div class="row" style="margin-top: 10px">
+                <div class="col-md-3">
+                  <strong><i class="fa fa-link  margin-r-5"></i>Details</strong>                
+                 </div>
+                  <div class="col-md-9">
+                    <a href="{{asset($projet->detail)}}">Lien fichier</a>
+                  </div> 
+                </div> 
+                @endif
+              
+              
+            <!-- /.box-body -->
+            </div>
+          
+         </div><!-- /.container -->
+      </div>
+</div>
+</div>
 
 @endsection
