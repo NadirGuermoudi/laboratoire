a34367f0a445e5b1637849b3d6978f8b56bd354a
NadirGuermoudi <nadir.guermoudi@gmail.com>
2018-12-30 04:29:10 +0100

    linking between these and contact and fixing Authentification error

--- app/Contact.php
+++ app/Contact.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected $fillable = ['nom', 'prenom', 'partenaire_id', 'fonction', 'pays', 'ville', 'adresse', 'email', 'telephone', 'created_at','updated_at'];
+		protected $fillable = ['nom', 'prenom', 'partenaire_id', 'fonction', 'pays', 'ville', 'adresse', 'email', 'telephone', 'created_at','updated_at'];
 
-    public function partenaire(){
-    	return $this->belongsTo('App\Partenaire');
-    }
+		public function partenaire(){
+			return $this->belongsTo('App\Partenaire');
+		}
 }
--- app/These.php
+++ app/These.php
@@ -6,12 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class These extends Model
 {
-     use SoftDeletes;
+	use SoftDeletes;
 
-     protected $dates = ['deleted_at'];
+	protected $dates = ['deleted_at'];
 
-     public function user()
-    {
-        return $this->belongsTo('App\User');
-    }
+	public function user(){
+		return $this->belongsTo('App\User');
+	}
+
+	public function encadreurExterieur(){
+		return $this->belongsTo('App\Contact', 'encadreur_ext');
+	}
+
+	public function coEncadreurExterieur(){
+		return $this->belongsTo('App\Contact', 'coencadreur_ext');
+	}
 }
